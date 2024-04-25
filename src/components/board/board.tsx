15ce47de723d7eb76e5aa579a48bf13f1dd5e339
Roshan Singh <roshan2152@gmail.com>
2024-04-25 23:41:00 +0530

    Sidebar

---
+++ src/components/board/board.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Board() {
+  return (
+    <div className='w-[80vw] h-full'>BOARD</div>
+  )
+}
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,22 @@
 import { User } from "firebase/auth";
 import Navbar from "@/components/navigationBar/navbar";
+import Sidebar from "@/components/sidebar/sidebar";
+import Board from "./board/board";
+
 interface HomeProps {
-    user:User;
+    user: User;
 }
 
-export const Home = ({user}:HomeProps) => {
+export const Home = ({ user }: HomeProps) => {
     return (
-        <div>
+        <div className="h-screen">
             <Navbar />
+            <div className="flex flex-row pt-20 h-full">
+                <div className="hidden md:flex">
+                    <Sidebar />
+                </div>
+                <Board />
+            </div>
         </div>
     )
 }
